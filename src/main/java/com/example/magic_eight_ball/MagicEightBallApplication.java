b8f63ab3b3248ee97d72ea7364c2a4af3b540644
Justin <justinli.9876@gmail.com>
2022-04-15 23:44:48 -0400

    First commit

---
+++ src/main/java/com/example/magic_eight_ball/MagicEightBallApplication.java
@@ -0,0 +1,13 @@
+package com.example.magic_eight_ball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicEightBallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicEightBallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/magic_eight_ball/MagicEightBallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.magic_eight_ball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicEightBallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
